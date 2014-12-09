fb55cd25ce3c63f66ee5573fe870260ed03c1fb8
troy betz <troybetz@gmail.com>
2014-12-09 03:36:35 -0600

    add `embedUrl`

---
+++ test/embed-url-test.js
@@ -0,0 +1,81 @@
+/**
+ * Module dependencies
+ */
+
+var assert = require('assert');
+var proxyquire = require('proxyquireify')(require);
+
+var noembedResponse = require('./helpers/noembed-response');
+var embedHTML;
+var container;
+var embedUrl;
+
+describe('embed-url', function() {
+  beforeEach(function() {
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    /**
+     * Fake html returned from media-embed
+     */
+    
+    embedHTML = document.createElement('iframe');
+    embedHTML.src = 'http://www.youtube.com/embed/bDOYN-6gdRE';
+
+    var mediaEmbedStub = function(url, cb) {
+      return cb(null, embedHTML, noembedResponse);
+    };
+
+    /**
+     * Magic happens
+     */
+     
+    embedUrl = proxyquire('../lib/embed-url', {
+      'media-embed': mediaEmbedStub
+    });
+  });
+
+  afterEach(function() {
+    document.body.removeChild(container);
+  });
+
+  it('embeds a `url`', function(done) {
+    embedUrl(noembedResponse.url, container, function() {
+      assert.ok(container.querySelector('iframe'));  
+      assert.ok(/bDOYN-6gdRE/.test(container.querySelector('iframe').src));
+      done();
+    });
+  });
+
+  it('adds an id of `media-embed` to the embed', function(done) {
+    embedUrl(noembedResponse.url, container, function() {
+      assert.equal(container.querySelector('iframe').id, 'media-embed');  
+      done();
+    });
+  });
+
+  it('should return the data used to embed `url`', function(done) {
+    embedUrl(noembedResponse.url, container, function(err, data) {
+      assert.equal(data, noembedResponse);
+      done();
+    });;
+  });
+
+  it('should return an error if embed fails', function(done) {
+    var mediaEmbedStub = function(url, cb) {
+      var err = new Error('valid url required for embedding');
+      return cb(err);
+    };
+
+    var embedUrl = proxyquire('../lib/embed-url', {
+      'media-embed': mediaEmbedStub
+    });
+
+    embedUrl(noembedResponse.url, container, function(err, data) {
+      assert.ok(err !== null);
+      assert.ok(/valid url required/.test(err.message));
+      done();
+    });
+  });
+});
---
+++ test/helpers/noembed-response.js
@@ -0,0 +1,20 @@
+/**
+ * Fake response retrieved from noembed.com
+ */
+
+module.exports = {
+  "width" : 425,
+  "author_name" : "schmoyoho",
+  "author_url" : "http://www.youtube.com/user/schmoyoho",
+  "version" : "1.0",
+  "provider_url" : "http://www.youtube.com/",
+  "provider_name" : "YouTube",
+  "thumbnail_width" : 480,
+  "thumbnail_url" : "http://i3.ytimg.com/vi/bDOYN-6gdRE/hqdefault.jpg",
+  "height" : 344,
+  "thumbnail_height" : 360,
+  "html" : "<iframe type='text/html' width='425' height='344' src='http://www.youtube.com/embed/bDOYN-6gdRE' frameborder=0></iframe>",
+  "url" : "http://www.youtube.com/watch?v=bDOYN-6gdRE",
+  "type" : "rich",
+  "title" : "Auto-Tune the News #8: dragons. geese. Michael Vick. (ft. T-Pain)"
+};
