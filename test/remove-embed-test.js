3cdeab50d8df7433ed6840c2f063b2678d66926e
troy betz <troybetz@gmail.com>
2014-12-10 06:30:32 -0600

    add `remove-embed`

---
+++ test/remove-embed-test.js
@@ -0,0 +1,27 @@
+/**
+ * Module dependencies
+ */
+
+var assert = require('assert');
+
+var removeEmbed = require('../lib/remove-embed');
+
+describe('remove-embed', function() {
+  beforeEach(function() {
+    iframe = document.createElement('iframe');
+    iframe.id = 'media-embed';
+
+    container = document.createElement('div');
+    container.appendChild(iframe);
+    document.body.appendChild(container);
+  });
+
+  afterEach(function() {
+    document.body.removeChild(container);
+  });
+
+  it('should remove an embedded iframe its container', function() {
+    removeEmbed('media-embed', container);
+    assert.equal(container.querySelector('#media-embed'), null);
+  });
+});
